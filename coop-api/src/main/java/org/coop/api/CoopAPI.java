8019355c65506528cac505e5a47c243dcc394d1b
Rodel Castro <rodelgcastro@gmail.com>
2018-03-08 02:19:42 +0800

    Initial Commit

---
+++ coop-api/src/main/java/org/coop/api/CoopAPI.java
@@ -0,0 +1,12 @@
+package org.coop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopAPI {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopAPI.class, args);
+	}
+}
---
+++ coop-api/src/test/java/org/coop/api/CoopAPITests.java
@@ -0,0 +1,16 @@
+package org.coop.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoopAPITests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
